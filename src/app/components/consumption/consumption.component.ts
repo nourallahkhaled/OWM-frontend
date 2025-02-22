0fedae69926d66ae07ef6262fbfc3e599a63eb65
nourallahkhaled <nourallahkhaled99@gmail.com>
2025-02-23 01:44:31 +0200

    fix websocket error

--- src/app/components/consumption/consumption.component.ts
+++ src/app/components/consumption/consumption.component.ts
@@ -72,7 +72,7 @@ export class ConsumptionComponent {
   }
 
   private subscribeToConsumption() {
-    const brokerUrl = 'wss://owmmeter.com:9001/mqtt';
+    const brokerUrl = 'wss://owmmeter.com/mqtt';
     this.mqttClient = mqtt.connect(brokerUrl);
 
     this.mqttClient.on('connect', () => {
--- src/app/components/leakage/leakage.component.ts
+++ src/app/components/leakage/leakage.component.ts
@@ -101,7 +101,7 @@ export class LeakageComponent implements OnInit, OnDestroy {
 
 getLeakageData() {
   // For WebSocket connection
-  const brokerUrl = 'wss://owmmeter.com:9001/mqtt';
+  const brokerUrl = 'wss://owmmeter.com/mqtt';
 
   this.mqttClient = mqtt.connect(brokerUrl);
 
